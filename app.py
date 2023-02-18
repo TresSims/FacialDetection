8f540df521ff4ba0cc1660c36ff243de885f26d2
Tres Sims <tres.sims95@gmail.com>
2023-02-18 14:56:26 -0500

    Initial Commit

--- app.py
+++ app.py
@@ -4,16 +4,22 @@ import shutil
 
 training_file = "./TrainingData/Cal_Sims.jpg"
 input_directory = "./InputData/"
-output_driectory = "./MatchedData/"
+output_directory = "./MatchedData/"
 
 
 known_image = face_recognition.load_image_file(training_file)
 
 
 cal_encoding = face_recognition.face_encodings(known_image)[0]
+print(cal_encoding)
 for unknown_file in os.listdir(input_directory):
-    unknown_image = face_recognition.load_image_file(training_file)
-    for encoding in face_recognition.face_encoding(unknown_image)[0]:
+    faces = 1
+    unknown_image = face_recognition.load_image_file(input_directory + unknown_file)
+    for encoding in face_recognition.face_encodings(unknown_image):
+        print(f"Checking face {faces} in file {unknown_file}")
+        faces += 1
         result = face_recognition.compare_faces([cal_encoding], encoding)
-        if result:
-            shutil.copyfile(unknown_file, output_directory)
+        print(result)
+        if result[0]:
+            print("Copying file")
+            shutil.copyfile(input_directory + unknown_file, output_directory + unknown_file)
